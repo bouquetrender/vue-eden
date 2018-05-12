53df5a0cc00d8f11dab00ed4fcfc52db93d5530c
Sakuya <502566796@qq.com>
2018-05-12 12:04:22 +0800

    feat: update dep

--- vue.config.js
+++ vue.config.js
@@ -34,19 +34,12 @@ module.exports = {
   productionSourceMap: true,
   // CSS 相关选项
   css: {
-    // 将组件内的 CSS 提取到一个单独的 CSS 文件 (只用在生产环境中)
-    extract: true,
-
     // 是否开启 CSS source map？
     sourceMap: false,
 
     // 为预处理器的 loader 传递自定义选项。比如传递给
     // sass-loader 时，使用 `{ sass: { ... } }`。
-    loaderOptions: {},
-
-    // 为所有的 CSS 及其预处理文件开启 CSS Modules。
-    // 这个选项不会影响 `*.vue` 文件。
-    modules: false
+    loaderOptions: {}
   },
 
   devServer: {
